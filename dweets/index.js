1d75c8e71ff5c05b846db4677b0f3348fd6fa50f
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-07-04 21:25:20 +0530

    Created dweet schema

--- dweets/index.js
+++ dweets/index.js
@@ -9,4 +9,24 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect("mongodb+srv://admin-parav:Test123@cluster0.dflwu.mongodb.net/dweetsDB", { useNewUrlParser: true, useUnifiedTopology: true });
-console.log("Connected to DB");
+
+const dweetSchema = new mongoose.Schema({
+    dweet: {
+        type: String,
+        required: [true, "Please check your data entry, no content entered!"]
+    },
+    posted_by: {
+        type: String,
+        required: [true, "Please check your data entry, no username entered"]
+    },
+    posted_at: {
+        type: Date,
+        required: [true, "Please check your data entry, no date specified!"]
+    },
+    last_updated_at: {
+        type: Date,
+        required: [true, "Please check your data entry, no date specified!"]
+    }
+});
+
+const Dweet = mongoose.model("Dweet", dweetSchema);
