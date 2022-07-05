1ddf08ab4be22e58409f0ab45af1fa00fc66c1fa
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-07-05 22:45:44 +0530

    Created and implemented 'Header' component

--- dweets/src/App.js
+++ dweets/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Header from './components/Header/header';
 
 export default function App() {
   return (
     <div className="App">
+      <Header/>
     </div>
   );
 }
