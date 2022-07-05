a951875e443799eaa43869decd93a85d90b28cc4
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-07-06 00:02:27 +0530

    Changed styling and minor edits

--- dweets/src/App.js
+++ dweets/src/App.js
@@ -1,10 +1,22 @@
 import './App.css';
+import { useState } from "react";
 import Header from './components/Header/header';
+import Menu from './components/Menu/menu';
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const baseLink = "https://dweets-api.herokuapp.com";
+
+  fetch(`${baseLink}/dweet`)
+    .then(res => res.json())
+    .then(result => setData(result))
+
   return (
     <div className="App">
       <Header/>
+      <div class="bottom-section">
+        <Menu/>
+      </div>
     </div>
   );
 }
