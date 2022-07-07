6711eb17668358ab0133c110ce548cc1c5b54a85
Parav Jaikrishan <106922327+parav-jaikrishan@users.noreply.github.com>
2022-07-08 00:28:28 +0530

    Delete dwitter directory

--- dwitter/src/components/CreateButton/createbtn.js
+++
@@ -1,8 +0,0 @@
-import "./createbtn.css";
-import { Link } from "react-router-dom";
-
-export default function CreateButton() {
-    return (
-        <Link to="/create"><button className="create-btn">+</button></Link>
-    )
-}
