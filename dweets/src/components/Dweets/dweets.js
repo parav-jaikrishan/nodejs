eb1b5a3f7e8f76086f0cd2c8501480367786d669
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-07-08 01:27:06 +0530

    Names with length > 20 are shortened

--- dweets/src/components/Dweets/dweets.js
+++ dweets/src/components/Dweets/dweets.js
@@ -7,9 +7,12 @@ export default function Dweets(props) {
     return(
         <div className="dweets-container">
             {dweets.map(dweet => {
+                if(dweet.posted_by.length > 20) {
+                    dweet.posted_by = `${dweet.posted_by.substring(0, 20)}...`
+                }
                 return(
                     <div key={dweet._id}>
-                        <h1>{dweet.posted_by} <span>@{dweet.posted_by.toLowerCase().replace(/[` ~!@#$%^&*()_|+\-=?;:'",.<>\\{\\}\\[\]\\\\/]/gi, '')}</span></h1>
+                        <h1>{dweet.posted_by} <span>@{dweet.posted_by.toLowerCase().replace(/[` ~!@#$%^&*()|+\-=?;:'",<>\\{\\}\\[\]\\\\/]/gi, '')}</span></h1>
                         <p style={{whiteSpace: "pre-wrap"}} className="dweet">{dweet.dweet}</p>
                         <p className="post-info">Posted {moment(dweet.posted_at).format("hh:mm A, Do MMM YYYY")} <span className={dweet.posted_at === dweet.last_updated_at ? "hide": "show"}>&#x2022; Edited {moment(dweet.last_updated_at).format("hh:mm A, Do MMM YYYY")}</span></p>
                         <div className="separate"></div>
