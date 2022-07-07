11beb4add10dae91ef0984ac4676570624a413e0
Parav Jaikrishan <r21947parav@dpsrkp.net>
2022-07-07 22:27:20 +0530

    Completed 'Dweets' component

--- dwitter/src/App.js
+++ dwitter/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { useState } from "react";
 import Header from './components/Header/header';
 import Dweets from './components/Dweets/dweets';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 export default function App() {
   const [data, setData] = useState([]);
   const baseLink = "https://dweets-api.herokuapp.com";
@@ -11,12 +11,29 @@ export default function App() {
     .then(res => res.json())
     .then(result => setData(result))
 
+  function deleteFromId(id) {
+    const deletedID = { id };
+    fetch(`${baseLink}/dweet/${id}`, {
+      method: 'DELETE',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(deletedID)
+    }).then(() => console.log(`deleted dweet`))
+  }
   return (
-    <div className="App">
-      <Header/>
-      <div className='bottom-section'>
-        <Dweets dweets={data}/>
+    <Router>
+      <div className="App">
+        <Header/>
+        <div className='bottom-section'>
+          <Switch>
+            <Route exact patch="/">
+              <Dweets dweets={data} handleDelete={i => deleteFromId(i)}/>
+            </Route>
+            <Route path="/update/:id">
+              
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
